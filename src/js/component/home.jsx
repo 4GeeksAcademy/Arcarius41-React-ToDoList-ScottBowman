99c98a608d3d9b3a79dda2244266a4f145188fba
Arcarius41 <145110529+Arcarius41@users.noreply.github.com>
2023-12-05 01:38:35 +0000

    working on it

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ inputValue, setInputValue] = useState("")
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">blah blah</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="text-center mt-5">Todo's List</h1>
+			<ul>
+				<li>
+					<input type="text"
+					onChange={(e) => setInputValue(e.target.value)}
+					value = {inputValue}
+					onKeyPress= {(e) => e.keyCode == 13 ? setTodos}
+					placeholder="What do you want to do?" />
+				</li>
+				<li>ahh</li>
+			</ul>
+
 		</div>
 	);
 };
